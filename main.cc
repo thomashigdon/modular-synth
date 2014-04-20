35d97c320316de0b1b3c1acb922e674a66061f60
Thomas Higdon <thomas.p.higdon@gmail.com>
2014-04-19 20:43:11 -0400

    whitespace

--- main.cc
+++ main.cc
@@ -10,24 +10,23 @@ using namespace std;
 
 int main()
 {
-	Params params;
-	params.SampleRate = 48000;
-	Synth synth(params);
+  Params params;
+  params.SampleRate = 48000;
+  Synth synth(params);
   NodeList temp((Node *)0);
   SquareGenerator squaregen(&synth, 2, .25, 262, 0.5, NodeList(0));
   Attenuator attenuator(&synth, 1, 1, NodeList(&squaregen), NodeList(0)); 
-	SineGenerator sinegen(&synth, 1, 0, 524, NodeList(0));
+  SineGenerator sinegen(&synth, 1, 0, 524, NodeList(0));
   NodeList adderInput(&sinegen, &attenuator);
   Adder adder(&synth, 0, adderInput, temp);
   synth.AddNode(&adder);
-	synth.AddNode(&sinegen);
+  synth.AddNode(&sinegen);
   synth.AddNode(&attenuator);
-	synth.AddNode(&squaregen);
+  synth.AddNode(&squaregen);
 
-	JackOut out(&synth);
+  JackOut out(&synth);
 
   sleep(10);
 
-
-	return 0;
+  return 0;
 }
